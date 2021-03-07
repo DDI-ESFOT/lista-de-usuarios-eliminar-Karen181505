36359473237ccfb6e749c348c2736ba9d7034d22
Karen181505 <karen.hurtado@epn.edu.ec>
2021-03-07 16:37:24 -0500

    lista

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -30,13 +30,17 @@ const UserList = ({ users }) => {
     document.querySelector("#lastname").value = "";
   };
 
+  const handleEraseUser = (event) => {
+    
+  }
+
   return (
     <>
       <h1>Lista de usuarios ({usersList.length} usuarios)</h1>
       <input type="text" id="name" placeholder="Ingrese un nombre" />
       <input type="text" id="lastname" placeholder="Ingrese un apellido" />
       <button onClick={handleAddUser}>Añadir</button>
-      <button>Eliminar el último usuario</button>
+      <button onClick={handleEraseUser}>Eliminar el último usuario</button>
       <ul>
         {usersList.map((user, index) => {
           return <li key={Math.random()}>{formatName(user)}</li>;
